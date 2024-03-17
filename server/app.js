df06ad0788ee119b703c6ed55c1b02bd3e60ac9f
Nilesh Deshpande <nileshdeshpandework@gmail.com>
2024-03-17 08:33:07 +0530

    trying to fix cors issue

--- server/app.js
+++ server/app.js
@@ -7,30 +7,13 @@ const { cloudinaryConnect } = require("./Config/cloudinary");
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
 
-app.use(express.json());
-
-// importing routes
-const userRoutes = require("./Routes/user");
-const jobRoutes = require("./Routes/jobs");
-
-// defining routes
-app.use("/api/v1/auth", userRoutes);
-app.use("/api/v1/jobs", jobRoutes);
-
 dotenv.config();
 database.connect();
 
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.options("*", cors()); // Respond to preflight requests with CORS headers
-app.use(
-  cors({
-    origin: "*", // Allow requests from any origin
-    credentials: true,
-    methods: ['GET', 'POST'],
-  })
-);
+app.use(cors()); // Enable CORS for all routes
 app.use(
   fileUpload({
     useTempFiles: true,
@@ -40,11 +23,15 @@ app.use(
 
 cloudinaryConnect();
 
+// importing routes
+const userRoutes = require("./Routes/user");
+const jobRoutes = require("./Routes/jobs");
+
+// defining routes
+app.use("/api/v1/auth", userRoutes);
+app.use("/api/v1/jobs", jobRoutes);
+
 const PORT = process.env.PORT || 8000;
-
-// CORS preflight handling for all routes
-app.options("*", cors()); // Respond to preflight requests with CORS headers
-
 
 // Route to check service status
 app.get("/", (req, res) => {
